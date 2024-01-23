f0f9dc0d55c870b433a876c6fe54dc903b11061e
qarasuv <bayram.ali19997@gmail.com>
2024-01-23 14:08:04 +0000

    add history and editing transactions

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 
 from .views import *
 
 
 urlpatterns = [
     path('transactions/add/', TransactionAdd.as_view(), name='transactions-add'),
+    path('transactions/history/', TransactionHistory.as_view(), name='transactions-history'),
+    path('transactions/<int:pk>/', TransactionEdit.as_view(), name='transactions-edit'),
 ]

be080a7aac1f518594a2043a73a11533a25e413a
qarasuv <bayram.ali19997@gmail.com>
2024-01-24 15:27:28 +0000

    add report transaction

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('transactions/add/', TransactionAdd.as_view(), name='transactions-add'),
     path('transactions/history/', TransactionHistory.as_view(), name='transactions-history'),
     path('transactions/<int:pk>/', TransactionEdit.as_view(), name='transactions-edit'),
+    path('report/generate/', ReportGenerate.as_view(), name='report-generate'),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.shortcuts import render
 from django.views.generic import CreateView, ListView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
@@ -20,6 +23,27 @@ class TransactionHistory(LoginRequiredMixin, ListView):
         user = self.request.user
         queryset = Transaction.objects.filter(author=user)
         return queryset
+
+
+class ReportGenerate(ListView):
+    template_name = 'report.html'
+    context_object_name = 'report_data'
+
+    def post(self, request, *args, **kwargs):
+        date_range = self.request.POST.get('date_range')
+        report_data = self.get_queryset(date_range)
+        return render(request, self.template_name, {'report_data': report_data, 'date_range': date_range})
+
+    def get_queryset(self, date_range=None):
+        if date_range:
+            from_date, to_date = map(lambda x: datetime.strptime(x.strip(), '%m/%d/%Y'), date_range.split('-'))
+            from_date_str = from_date.strftime('%Y-%m-%d')
+            to_date_str = to_date.strftime('%Y-%m-%d')
+            user = self.request.user
+            queryset = Transaction.objects.filter(author=user, date__range=(from_date_str, to_date_str))
+            return queryset
+        else:
+            pass
 
 
 class TransactionEdit(UpdateView):
